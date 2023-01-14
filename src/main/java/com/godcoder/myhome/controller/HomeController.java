81f6447e64bd35cec1a660c7910d0b4de06ab200
SEUL <zpdlxk1216@hanmail.net>
2023-01-14 19:04:55 +0900

    게시판 리스트작성

--- src/main/java/com/godcoder/myhome/controller/HomeController.java
+++ src/main/java/com/godcoder/myhome/controller/HomeController.java
@@ -1,4 +1,4 @@
-package com.godcoder.myhome.Controller;
+package com.godcoder.myhome.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
