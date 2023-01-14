ceef99509883aebcd88ec4467be15b870b39ff57
SEUL <zpdlxk1216@hanmail.net>
2023-01-14 23:49:41 +0900

    게시판 유효성검사중 중단

--- src/main/java/com/godcoder/myhome/model/Board.java
+++ src/main/java/com/godcoder/myhome/model/Board.java
@@ -1,12 +1,17 @@
 package com.godcoder.myhome.model;
 
 
+
+
 import lombok.Data;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 
 @Entity
 @Data
@@ -15,6 +20,8 @@ public class Board {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @NotNull
+    @Size(min=2, max=30)
     private String title;
     private String content;
 }
