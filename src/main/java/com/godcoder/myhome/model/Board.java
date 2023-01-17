b74a3c8d847c7a13d9a266be7d18b2c4b0eb7b6c
Your Name <zpdlxk1216@hanmail.net>
2023-01-17 13:30:34 +0900

    페이지 목록작성 및 검색태그 추가

--- src/main/java/com/godcoder/myhome/model/Board.java
+++ src/main/java/com/godcoder/myhome/model/Board.java
@@ -5,10 +5,7 @@ package com.godcoder.myhome.model;
 
 import lombok.Data;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
